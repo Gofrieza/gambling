2f90869124a7ff7efd1a2c8f438def131498fdc8
Gofrieza <93874332+Gofrieza@users.noreply.github.com>
2022-01-31 18:48:41 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using casino;
+
+Console.WriteLine("Hello, World!");
+gambler.bet();
---
+++ gambler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace casino
+{
+    public static void Gambler()
+    {
+        double totalMoney = 100.00;
+        double betMoney = 1.00;
+    }
+}
